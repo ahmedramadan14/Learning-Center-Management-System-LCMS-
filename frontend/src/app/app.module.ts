65553bf77f3aa66950b6a9c007573f5592a8a11f
suhailamahfouz <2401411@rst.edu.eg>
2026-08-10 17:40:27 +0300

    Add frontend structure

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,50 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FormsModule } from '@angular/forms';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { DashboardHomeComponent } from './pages/dashboard/dashboard-home/dashboard-home.component';
+import { StudentsComponent } from './pages/dashboard/students/students.component';
+import { TeachersComponent } from './pages/dashboard/teachers/teachers.component';
+import { CoursesComponent } from './pages/dashboard/courses/courses.component';
+import { ClassesComponent } from './pages/dashboard/classes/classes.component';
+import { ParentsComponent } from './pages/dashboard/parents/parents.component';
+import { SecretariesComponent } from './pages/dashboard/secretaries/secretaries.component';
+import { AttendanceComponent } from './pages/dashboard/attendance/attendance.component';
+import { ExamsComponent } from './pages/dashboard/exams/exams.component';
+import { PaymentsComponent } from './pages/dashboard/payments/payments.component';
+import { SettingsComponent } from './pages/dashboard/settings/settings.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    LandingComponent,
+    DashboardHomeComponent,
+    StudentsComponent,
+    TeachersComponent,
+    CoursesComponent,
+    ClassesComponent,
+    ParentsComponent,
+    SecretariesComponent,
+    AttendanceComponent,
+    ExamsComponent,
+    PaymentsComponent,
+    SettingsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
